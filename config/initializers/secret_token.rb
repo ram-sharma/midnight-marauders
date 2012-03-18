fd435a30e8eea6cc86ba723b38e5efb93a5a25ff
Austin Lee D4L <Austin.L.D4L@gmail.com>
2012-03-17 20:06:33 -0700

    Created rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MidnightMarauders::Application.config.secret_token = '3bafd7758cfa01cc52a0e5571daebb3fbbb6068bc98b1c1511135f43440896b6306f8efb31be95a40c9efacffcff7e98c2a99fec755668e6d5b9dacea7d8130a'
