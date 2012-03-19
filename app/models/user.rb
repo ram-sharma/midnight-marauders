9f024cb50512617072b2348793bfe3b512db1a76
Austin Lee D4L <Austin.L.D4L@gmail.com>
2012-03-18 22:05:53 -0700

    added haml gem, controllers, some views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
+  validates_length_of :username, :within => 3..20
+  validates_length_of :pass, :within => 3..20
+  validates_uniqueness_of :username
+
   has_one :karma, :as => :karmable
 end
