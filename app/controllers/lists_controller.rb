e26e8cae5b8ccaab7180526eb492f37a3a798b53
Austin Lee D4L <Austin.L.D4L@gmail.com>
2012-03-20 21:32:08 -0700

    Add new lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,24 @@
 class ListsController < ActionController::Base
+  def new
+    @list = List.new
+
+    respond_to do |format|
+      format.html
+      format.json {render :json => @post}
+    end
+  end
+
+  def create
+    @list = List.new(params[:post])
+
+    respond_to do |format|
+      if @list.save
+        format.html { redirect_to(@list, :notice => 'list was successfully created')}
+        format.json { render :json => @post, :status => :created, :location => @post }
+      else
+        format.html { render :action => "new"}
+        format.json { render :json => @post.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
 end
